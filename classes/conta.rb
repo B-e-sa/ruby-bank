8438cf35b149d82a0a9d05db82d9902864bcaa9d
Besa <joaospereira963@gmail.com>
2023-07-24 11:08:48 -0300

    init conta class logic

--- classes/conta.rb
+++ classes/conta.rb
@@ -1,15 +1,17 @@
 def Conta
     attr_reader :saldoDisponivel, :faturaAtual, :usuario
 
-    def initialize
-
+    def initialize(usuario, saldoDisponivel = 0)
+        @usuario = usuario
+        @saldoDisponivel = saldoDisponivel
+        @faturaAtual = 0
     end
 
     def adicionarSaldo(valor)
-
+        @saldoDisponivel += valor
     end
 
     def setFaturaAtual(valor)
-
+        @faturaAtual += valor
     end
 end 
