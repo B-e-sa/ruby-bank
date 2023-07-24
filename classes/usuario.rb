14086895e96288cc92523655f0cc83bda046f476
Besa <joaospereira963@gmail.com>
2023-07-24 11:16:22 -0300

    init usuario logic

--- classes/usuario.rb
+++ classes/usuario.rb
@@ -1,11 +1,16 @@
 class Usuario
     attr_reader :nome, :cpf, :cartoes
 
-    def initialize
-    
+    def initialize(nome, cpf)
+        @nome = nome
+        @cpf = cpf
     end
 
     def trocarNome(novoNome)
+        @nome = novoNome
+    end
 
+    def adicionarCartao(Cartao)
+        cartoes.push(Cartao)
     end
 end
