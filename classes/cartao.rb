9b9f607296653fd3a68accec5b45dc6a646efe73
Besa <joaospereira963@gmail.com>
2023-07-24 11:06:28 -0300

    add aditional cartao methods

--- classes/cartao.rb
+++ classes/cartao.rb
@@ -1,14 +1,29 @@
 class Cartao
     attr_reader :numero, :validade, :cvc, :funcao, :isBloqueado, :usuario
 
-    def initialize
+    def initialize(usuario)
+
     end
 
     def trocarStatusBloqueado()
 
     end
 
-    def trocarFuncao() 
+    def trocarFuncaoAtual() 
+
+    end
+
+    protected
+
+    def definirValidade()
+
+    end
+
+    def gerarCvc()
+        
+    end
+
+    def gerarNovoNumero()
 
     end
 end
