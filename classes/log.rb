6d4d9bdaae375c65cc9299b8cc53a5997f10f589
Besa <joaospereira963@gmail.com>
2023-07-25 11:53:37 -0300

    add extra log methods

--- classes/log.rb
+++ classes/log.rb
@@ -15,15 +15,41 @@ class Log
         puts 'Escolha seu novo nome de usuário:'
     end
 
+    def self.line 
+        puts("--------------------------------------")
+    end
+
+    def self.get_input
+        return gets.chomp
+    end
+
+    def self.get_int
+        return gets.chomp.to_i
+    end
+
+    def self.usuario(usuario)
+        puts "Nome: #{usuario.nome}"
+        puts "Cpf: #{usuario.cpf}"
+    end
+
     def self.opcoes
         puts '1: Meu usuário'
         puts '2: Minha Conta'
         puts '3: Meus cartões'
+        puts '4: Encerrar a aplicação'
     end
 
     def self.opcoes_usuario
         puts '1: Alterar meu nome'
         puts '2: Excluir meu usuário'
+    end
+
+    def self.escolha_nome
+        print('Escolha seu novo nome: ')
+    end
+
+    def self.nome_alterado_sucesso(nome)
+        print("Seu nome foi alterado com sucesso, #{nome}!")
     end
 
     def self.opcoes_conta
