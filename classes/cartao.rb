9d2ac9b9aad0c975d80cb53936830d7952508f08
Joao Pereira <joao.pereira@precato.com.br>
2023-08-04 09:38:33 -0300

    feat: remove params and methods curly brackets

--- classes/cartao.rb
+++ classes/cartao.rb
@@ -4,22 +4,22 @@ class Cartao
     include TipoCartao
     attr_reader :numero, :validade, :cvc, :funcao, :is_bloqueado, :usuario
 
-    def initialize(usuario, funcao = TipoCartao::CREDITO)
+    def initialize(funcao = TipoCartao::CREDITO)
         @users = App.instance
         @numero = gerar_novo_numero()
         @validade = definir_validade()
         @cvc = gerar_cvc()
         @funcao = funcao
         @is_bloqueado = false
-        @usuario = usuario
+        @usuario = App.instance.current_user
         vincular_usuario()
     end
 
-    def trocarStatusBloqueado()
+    def trocarStatusBloqueado
         @is_bloqueado = !@is_bloqueado
     end
 
-    def trocarFuncaoAtual()
+    def trocarFuncaoAtual
         if @funcao == TipoCartao::CREDITO
             @funcao = TipoCartao::DEBITO
         else
@@ -30,21 +30,21 @@ class Cartao
     protected
 
     # TODO: adicionar ao diagrama
-    def vincular_usuario()
+    def vincular_usuario
         usuario.adicionar_cartao(self)
     end
 
-    def definir_validade()
+    def definir_validade
         data_de_validade = Time.now.year + 5
         return data_de_validade
     end
 
-    def gerar_cvc()
+    def gerar_cvc
         cvc = "#{rand(10)}#{rand(10)}#{rand(10)}"
         return cvc
     end
 
-    def gerar_novo_numero()
+    def gerar_novo_numero
         numeros = ''
 
         16.times do
