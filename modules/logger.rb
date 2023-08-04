bdc251d595577baf7d5025424dcb663dc78e8dc2
Joao Pereira <joao.pereira@precato.com.br>
2023-08-04 09:36:57 -0300

    feat: switch app param to app instance and add new logs

--- modules/logger.rb
+++ modules/logger.rb
@@ -1,71 +1,4 @@
 module Logger
-    def self.log message
-        puts message
-    end
-
-    def self.log_line message
-        print message
-    end
-
-    module User
-        def self.informacao_usuario usuario
-            puts "> Usuário"
-            puts "  - Nome: #{usuario.nome}"
-            puts "  - CPF: #{usuario.cpf}"
-        end
-
-        def self.tem_cer1teza
-            puts '1: Sim'
-            puts '2: Não'
-        end
-
-        def self.deseja_continuar
-            puts "Deseja continuar?"
-            puts "1: Sim"
-            puts "2: Não, sair"
-        end
-
-        def self.nenhum_usuario_cadastrado
-            puts "Ainda não há usuários cadastrados. Deseja criar um?"
-            puts "1: Sim"
-            puts "2: Não, sair"
-        end
-
-        def self.opcoes
-            puts '1: Alterar meu nome'
-            puts '2: Excluir meu usuário'
-        end
-
-        def self.mostrar_usuario(usuario)
-            puts "Nome: #{usuario.nome}"
-            puts "Cpf: #{usuario.cpf}"
-        end
-    end
-
-    module Cartao
-        def self.opcoes
-            puts '> Cartão'
-            puts '1: Consultar informações de um cartão'
-            puts '2: Criar novo cartão'
-        end
-    end
-
-    module Conta
-        def self.opcoes
-            puts '> Conta'
-            puts '1: Adicionar saldo'
-            puts '2: Retirar dinheiro'
-        end
-    end
-
-    def self.limpar
-        if Gem.win_platform?
-            system 'cls'
-        else
-            system 'clear'
-        end
-    end
-
     def self.introducao
         puts 'Olá! Bem-vindo(a) ao nosso banco. Em que podemos lhe ajudar?'
     end
@@ -94,5 +27,83 @@ module Logger
         puts '2: Minha Conta'
         puts '3: Meus cartões'
         puts '4: Encerrar a aplicação'
+    end
+
+    def self.log message
+        puts message
+    end
+
+    def self.log_line message
+        print message
+    end
+
+    def self.tem_certeza
+        puts '1: Sim'
+        puts '2: Não'
+    end
+
+    def self.deseja_continuar
+        puts "Deseja continuar?"
+        puts "1: Sim"
+        puts "2: Não, sair"
+    end
+
+    module User
+        def self.mostrar_cartoes
+            App.instance.current_user.cartoes.each do |cartao|
+                puts cartao.numero
+            end
+        end
+
+        def self.informacao_usuario
+            puts "> Usuário"
+            puts "  - Nome: #{App.instance.current_user.nome}"
+            puts "  - CPF: #{App.instance.current_user.cpf}"
+        end
+
+        def self.nenhum_usuario_cadastrado
+            puts "Ainda não há usuários cadastrados. Deseja criar um?"
+            puts "1: Sim"
+            puts "2: Não, sair"
+        end
+
+        def self.opcoes
+            puts '1: Alterar meu nome'
+            puts '2: Excluir meu usuário'
+        end
+
+        def self.mostrar_usuario
+            puts "Nome: #{App.instance.current_user.nome}"
+            puts "Cpf: #{App.instance.current_user.cpf}"
+        end
+    end
+
+    module Cartao
+        def self.opcoes
+            puts '> Cartão'
+            puts '1: Consultar informações de um cartão'
+            puts '2: Criar novo cartão'
+        end
+    end
+
+    module Conta
+        def self.informacao_conta
+            puts '> Conta'
+            puts "  - Saldo atual: #{App.instance.current_user.conta.saldo_disponivel}"
+            puts "  - Fatura atual: #{App.instance.current_user.conta.saldo_disponivel}"
+        end
+
+        def self.opcoes
+            puts '1: Adicionar saldo'
+            puts '2: Retirar dinheiro'
+        end
+    end
+
+    def self.limpar
+        if Gem.win_platform?
+            system 'cls'
+        else
+            system 'clear'
+        end
     end
 end
