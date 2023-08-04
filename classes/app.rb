c8f3581492cd6040e9ea0d9c7678b9281701880c
Joao Pereira <joao.pereira@precato.com.br>
2023-08-04 09:36:02 -0300

    feat: add register and login methods to app

--- classes/app.rb
+++ classes/app.rb
@@ -10,11 +10,46 @@ class App
     @current_user = nil
   end
 
-  def register user
+  def register
+    print "Seu nome: "
+    nome = Logger::get_input
+
+    print "Seu cpf: "
+    cpf = Logger::get_input
+
+    user = ::Usuario.new nome, cpf
+
     @users << user
+    @current_user = user
   end
 
-  def login user
+  def excluir_usuario
+    @users.delete(current_user)
+  end
+
+  def login
+    while true
+      Logger::log_line 'Para entrar, insira o CPF de sua conta: '
+
+      cpf = Logger::get_input
+
+      App.instance.users.each do |item|
+          if item.cpf == cpf
+              return item
+          end
+      end
+
+      Logger::limpar
+      Logger::log 'Usuário não encontrado'
+      Logger::User::deseja_continuar
+
+      if Logger::get_input == 2
+          return nil
+      end
+
+      Logger::limpar
+    end
+
     @current_user = user
   end
 end
