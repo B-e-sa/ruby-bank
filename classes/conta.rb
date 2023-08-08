554bce3ad0ce1e79e86af345372c0ad11f4e33b3
B-e-sa <joaospereira963@gmail.com>
2023-08-08 10:33:47 -0300

    fix: remove unused singleton instances, feat: add validations to the methods

--- classes/conta.rb
+++ classes/conta.rb
@@ -2,25 +2,19 @@ class Conta
     attr_reader :saldo_disponivel, :fatura_atual, :usuario
 
     def initialize(usuario, saldo_disponivel = 0)
-        @users = App.instance
         @usuario = usuario
         @saldo_disponivel = saldo_disponivel
-        @fatura_atual = 0
     end
 
     def adicionar_saldo valor
-        if valor == 0; return nil end
+        if valor <= 0; return nil end
 
         @saldo_disponivel += valor
     end
 
     def subtrair_saldo valor
-        if @saldo_disponivel < valor; return nil end
+        if valor > saldo_disponivel || valor <= 0; return nil end
 
         return @saldo_disponivel -= valor
-    end
-
-    def set_fatura_atual valor
-        @fatura_atual += valor
     end
 end
